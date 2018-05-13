0490b82ebbd812a24e8dc0cab7e1774a30d68546
rachit173 <rachittibrewal@outlook.com>
2018-05-13 16:40:20 +0530

    Beast and Asio added as include files

--- test.cpp
+++ test.cpp
@@ -4,6 +4,9 @@
 #include <mrpt/nav.h>
 #include <mrpt/slam.h>         // Include all classes in mrpt-slam and its dependencies
 #include <boost/system/error_code.hpp>         // Include all classes in mrpt-base and its dependencies
+#include <boost/beast.hpp>
+#include <include/boost/beast/http.hpp>
+#include <include/boost/beast/version.hpp>
 using namespace mrpt;
 using namespace mrpt::system; 
 // using namespace mrpt::slam;    // Select namespace for maps, observations, etc...

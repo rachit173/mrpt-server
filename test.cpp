71c6780ce071724d15618f0063c1b80c4e791c99
rachit173 <rachittibrewal@outlook.com>
2018-05-13 15:44:50 +0530

    Add Hunter to pull deps

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#define MRPT_NO_WARN_BIG_HDR
+#include <bits/stdc++.h>
+#include <mrpt/system/CTicTac.h>
+#include <mrpt/nav.h>
+#include <mrpt/slam.h>         // Include all classes in mrpt-slam and its dependencies
+#include <boost/system/error_code.hpp>         // Include all classes in mrpt-base and its dependencies
+using namespace mrpt;
+using namespace mrpt::system; 
+// using namespace mrpt::slam;    // Select namespace for maps, observations, etc...
+using namespace std;
+int main()
+{
+    return 0;
+}
